bf6decca7c1528921cb7d4bdd7ee069fa05d7584
kz422 <p.inoue.p@gmail.com>
2022-10-08 01:50:05 +0900

    Initial Commit

---
+++ movie_app_swift/movie_app_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_app_swiftApp.swift
+//  movie_app_swift
+//
+//  Created by Kazuki Inoue on 2022/10/08.
+//
+
+import SwiftUI
+
+@main
+struct movie_app_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
