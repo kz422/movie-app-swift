359c09e6b3943f8a9aeb403b367cd17444600ee3
kz422 <p.inoue.p@gmail.com>
2022-10-08 03:41:02 +0900

    initial commit

---
+++ movie_app_swift/Views/HomeTabView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTabView.swift
+//  movie_app_swift
+//
+//  Created by Kazuki Inoue on 2022/10/08.
+//
+
+import SwiftUI
+
+struct HomeTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeTabView()
+    }
+}
---
+++ movie_app_swift/Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  movie_app_swift
+//
+//  Created by Kazuki Inoue on 2022/10/08.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
